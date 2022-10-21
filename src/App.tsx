243887065156871050493e34eb52cf60af89177b
Therese <theresewiig1@gmail.com>
2022-10-21 16:06:26 +0200

    Going to start setting up the component for rendering pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,14 @@ import pokemon from "./data/pokemon.json";
 import { Col, Row } from "react-bootstrap";
 
 import "./fonts/Pokemon.ttf"; 
+import { Pokemon } from "./components/Pokemon";
 
 function App() {
 
   return (
     <>
-    <h1>hi!</h1>
-    <Row md={2} xs={1} lg={3} className="g-3">
-    {pokemon.map(item => (
-        <Col key={item.id}>
-           <img src={item.imageUrl}/>
-        </Col>
-    ))}
-</Row>
+    <h1>hi from app!</h1>
+    <Pokemon></Pokemon>
 </>
   )
 }
---
+++ src/components/Pokemon.tsx
@@ -0,0 +1,10 @@
+import "./Pokemon.css"; 
+
+export function Pokemon (){
+    return(
+        <>
+        <div className="test"></div>
+        <h2>hello from pokemon component</h2>
+        </>
+    )
+}
