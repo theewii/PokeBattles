ae7d4704f15f03e6dcbe5ba2d9e383b83146ff5f
Therese <theresewiig1@gmail.com>
2022-10-21 18:19:02 +0200

    completed setup with the grid - going to leave it with colors for a bit

--- src/components/BattleField.tsx
+++ src/components/BattleField.tsx
@@ -1,10 +1,23 @@
 
-import "./BattleField.scss"; 
+import "./BattleField.css"; 
 export function BattleField(){
     return (
         <div className="battleFieldContainer">
-            <div className="battleFieldGrid"></div>
-
+            <div className="item battleGrid">
+            <div className="item topLeft">TopLeft</div>   
+            <div className="item rivalScore">RivalScore</div>
+            <div className="item topRight">TopRight</div>
+            <div className="item midLeft1">midLeft1</div>
+            <div className="item mid1">mid1</div>
+            <div className="item midRight1">midRight1</div>
+            <div className="item midLeft2">midLeft2</div>
+            <div className="item mid2">mid2</div>
+            <div className="item midRight2">midRight2</div>
+            <div className="item bottomLeft">BottomLeft</div>
+            <div className="item playerScore">playerScore</div>
+            <div className="item bottomRight">BottomRight</div>
+            </div>
+        
         </div>
     )
 }
