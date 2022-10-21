298b2c92bbfdd15100d97739b218e75a603d7f57
Therese <theresewiig1@gmail.com>
2022-10-21 15:27:37 +0200

    Added pictures of all the pokemon, setup json data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import {Routes, Route} from "react-router-dom"; 
+import "./App.css";
+import pokemon from "./data/pokemon.json"; 
+import { Col, Row } from "react-bootstrap";
+
+import "./fonts/Pokemon.ttf"; 
+
 function App() {
 
   return (
+    <>
     <h1>hi!</h1>
+    <Row md={2} xs={1} lg={3} className="g-3">
+    {pokemon.map(item => (
+        <Col key={item.id}>
+           <img src={item.imageUrl}/>
+        </Col>
+    ))}
+</Row>
+</>
   )
 }
 
