56064b90cef5422772543b1dbe1fcb805a39d0f2
Therese Wiig <therese.wiig@triona.no>
2022-10-25 14:03:16 +0200

    About to do a refactor on rulesetNames -> commit

--- src/globalState/reducer.js
+++ src/globalState/reducer.js
@@ -1,10 +1,15 @@
+import PokemonData from "../data/pokemon.json";
+
 export const initialState = {
     playerScore: 0,
     rivalScore: 0,  
     playerPokemon: null, 
     rivalPokemon: null, 
     ruleset: null, 
-    isChoosingPokemon: false
+    isChoosingPokemon: false, 
+    defaultRuleSet: [], 
+    currentRuleSet: [], 
+    isUsingCustomRuleSet: false
 }
 
 const reducer = (state = initialState, action) => {
