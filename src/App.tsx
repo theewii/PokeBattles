abe939725f6f93fc7c6e54053743d71be7ff5a6f
Therese <theresewiig1@gmail.com>
2022-10-25 21:34:38 +0200

    still struggling to resolve the rival pokemon bug, makng a commit before a new try

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom"; 
 import "./App.css";
 import { Container } from "react-bootstrap";
+// @ts-ignore
 import composeWithGlobalState from './globalState/composeWithGlobalState';
 import "./fonts/Pokemon.ttf"; 
 import { BattleField } from "./components/BattleField";
--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,19 +1,13 @@
 import { Container } from "react-bootstrap";
-import { useScore } from "../hooks/hooks";
 import "./ScoreBoard.css";
 
-export function ScoreBoard({score, user}){
-    
-    //burde ha player score/rival score 
-    
+export function ScoreBoard({score, user} : {score: number, user: string}){
+
     return(
         <div className="paper">
             <Container className="p-2">
-       
                 <h2>{user}</h2>
                 <h3>{score}</h3>
- 
-
             </Container>
         </div>
     )
